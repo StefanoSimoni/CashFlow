d66493448695ab1b5719fe973c82dfc3d1ea69e8
Stefano Simoni <stefano.simoni.10@hotmail.com.ar>
2024-12-16 12:14:59 -0300

    Historial/Gripper

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <Layout>
-    <template #header> <Header /> </template>
+    <template #header><Header /></template>
+    <template #resume><Resume /></template>
+    <template #history><History /></template>
   </Layout>
 </template>
 
 <script setup>
 import Layout from "./Layout.vue";
 import Header from "./Header.vue";
+import Resume from "./Resume.vue";
+import History from "./History.vue";
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,12 +1,65 @@
 <template>
   <div class="layout">
     <slot name="header"></slot>
+    <div v-show="!showHistory">
+      <slot name="resume"></slot>
+    </div>
+    <div class="history">
+      <div class="header" @click="showHistory = !showHistory">
+        <span class="grip"></span>
+      </div>
+      <h1>History</h1>
+      <div class="body" v-show="showHistory">
+        <slot name="history"></slot>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
+<script setup>
+import { ref } from "vue";
+
+const showHistory = ref(false);
+</script>
+
+<style scoped>
 .layout {
+  height: 100vh;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.history {
+  width: 100vw;
+  box-shadow: #0689b0 0px 20px 70px;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  padding: 20px 0 0 0;
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  padding-left: 20px;
+}
+
+.header {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.grip {
+  width: 30%;
+  height: 10px;
+  background-color: #0689b0;
+  border-radius: 10px;
+}
+
+.body {
+  height: 85vh;
 }
 </style>
